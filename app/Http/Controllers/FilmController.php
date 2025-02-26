bdeb46b2ab2a5f7bff2aaa691f6371c6460786cc
bryanjrr <bryanjoyarubio@gmail.com>
2025-02-26 19:30:33 +0100

    #6 Film gender changed to a select option

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -169,6 +169,7 @@ class FilmController extends Controller
 
     public function createFilm(Request $request)
     {
+        dd($request);
         $title = "Crear Film";
         $films = FilmController::readFilms();
         $name = $request->input("name");
